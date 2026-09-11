8be0ebd13322c4b6ebd5f06e9adbb34b578b578d
alishakhan0413 <alishakhanak0413@gmail.com>
2026-09-11 16:47:43 +0500

    assignment

---
+++ Assignment 9/app.js
@@ -0,0 +1,6 @@
+// Declaration of empty variable
+var students = []
+// Declaration and initialization of strings in Array
+var students = ["Mushaim", "Mannan", "Hurain", "Rafay", "Haram"]
+// Declaration and initialization of numbers in Array
+var ageOfStudents = [17, 20, 6, 13, 24]
