961fc831ec4d8761b7f7d5d1c512ae934ba06516
alishakhan0413 <alishakhanak0413@gmail.com>
2026-09-11 17:11:23 +0500

    Java Assignments

--- Assignment 9/app.js
+++ Assignment 9/app.js
@@ -2,5 +2,14 @@
 var students = []
 // Declaration and initialization of strings in Array
 var students = ["Mushaim", "Mannan", "Hurain", "Rafay", "Haram"]
+console.log(`My name is ${students[0]}`);
 // Declaration and initialization of numbers in Array
 var ageOfStudents = [17, 20, 6, 13, 24]
+console.log(`My age is ${ageOfStudents[0]}`);
+// Boolean Array
+var booleanArr = [true,false,]
+// mixed Array
+var mixedArr = ["Alisha", 18, true, undefined, null]
+console.log(typeof mixedArr[2]);
+// Qualification Arrays
+var qualificationinPak = ["SSC", "HSC", "BCS", ""]
